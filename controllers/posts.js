b47fbad7e43c1cd21a92b9a7bc3d2dda023def08
mariscottin <nicolas.mariscotti93@gmail.com>
2019-07-26 20:55:04 -0300

    fix bug

--- controllers/posts.js
+++ controllers/posts.js
@@ -80,7 +80,7 @@ const uploadFile = (buffer, name, type) => {
                     date: new Date(),
                     section: fieldsSection
                     })
-                    .then(() => res.redirect('/admin/posts?alert=Novedad%20creada%20con%20exito'))
+                    .then(() => response.redirect('/admin/posts?alert=Novedad%20creada%20con%20exito'))
                     .catch(err=> console.log('could not add post: ' + err))
             } catch (error) {
                 return response.status(400).send(error);
