3e7273b6632eef8228e095587308f7640b98ffcd
mariscottin <nicolas.mariscotti93@gmail.com>
2019-07-29 16:57:11 -0300

    adding loading text

--- assets/js/upload-post.js
+++ assets/js/upload-post.js
@@ -1,11 +1,12 @@
 //IMAGE UPLOAD
 const submit = document.getElementById('submit-post-upload');
 const section = document.getElementById('post-section-id');
+const loadingText = document.querySelector('.loading-text');
 let newPostForm = document.getElementById('new-post-form');
 
 submit.addEventListener('click', (e)=> {
   e.preventDefault();
-  
+  loadingText.style.display = 'block';
   let formData = new FormData(newPostForm);
   formData.append('section', section.value);
 
