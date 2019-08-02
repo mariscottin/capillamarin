3ff0df03478f8453d7f800bb7a89de027bc09709
mariscottin <nicolas.mariscotti93@gmail.com>
2019-08-02 12:29:33 -0300

    testing if indexOf works

--- assets/js/edit-post.js
+++ assets/js/edit-post.js
@@ -4,7 +4,8 @@ const postTitle = document.getElementById('edit-post-title-id');
 const postBody = document.getElementById('edit-post-body-id');
 const loadingBtn = document.getElementById('loading-edit-btn');
 
-console.log(section.name);
+console.log(section.options.indexOf(section.name));
+
 submit.addEventListener('click', (e)=> {
     if(section.value === "default" || postTitle.value === "" || postBody.value === ""){
       alert('Por favor, completar todos los campos');
@@ -18,4 +19,4 @@ function setSelectedIndex(s, i){
   s.options[i-1].selected = true;
   return;
 }
-setSelectedIndex(document.getElementById("edit-post-section-id"),5);
+setSelectedIndex(document.getElementById("edit-post-section-id"), 5);
