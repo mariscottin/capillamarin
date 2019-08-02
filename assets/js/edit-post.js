cb75f6d0116c46accc945b01b6a040a0f91a804c
mariscottin <nicolas.mariscotti93@gmail.com>
2019-08-02 12:19:32 -0300

    testing with name

--- assets/js/edit-post.js
+++ assets/js/edit-post.js
@@ -4,7 +4,7 @@ const postTitle = document.getElementById('edit-post-title-id');
 const postBody = document.getElementById('edit-post-body-id');
 const loadingBtn = document.getElementById('loading-edit-btn');
 
-
+console.log(section.name);
 submit.addEventListener('click', (e)=> {
     if(section.value === "default" || postTitle.value === "" || postBody.value === ""){
       alert('Por favor, completar todos los campos');
@@ -13,3 +13,9 @@ submit.addEventListener('click', (e)=> {
       loadingBtn.style.display = 'block';
     }
 });
+
+function setSelectedIndex(s, i){
+  s.options[i-1].selected = true;
+  return;
+}
+setSelectedIndex(document.getElementById("edit-post-section-id"),5);
