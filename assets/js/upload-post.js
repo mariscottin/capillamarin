1758178b9770eed5e4a28cd34137d0373f87f0e4
mariscottin <nicolas.mariscotti93@gmail.com>
2019-09-17 16:15:09 -0300

    back to basics

--- assets/js/upload-post.js
+++ assets/js/upload-post.js
@@ -46,30 +46,7 @@ submit.addEventListener('click', (e)=> {
 
 
 //AUDIO UPLOAD
-submitAudio.addEventListener('click', (e)=> {
+submitAudio.addEventListener('click', (e) => {
   e.preventDefault();
   console.log('clicked');
-  if(audioTitle.value === "" || audioFile.value === "" || audioDate.value === ""){
-    alert('Por favor, completar todos los campos');
-  }else{
-    submitAudio.style.display = 'none';
-    loadingAudioBtn.style.display = 'block';
-
-    let formData = new FormData(newAudioForm);
-
-    axios({
-          method: 'post',
-          url: '/audios',
-          data: formData,
-      })
-       .then(response => {
-          console.log(response);
-          if(response){
-            window.location = '/admin/homilias/1?alert=Novedad%20creada%20con%20exito';
-          }
-  
-        }).catch(error => {
-          console.log(error);
-        });
-  }
-});
+})
