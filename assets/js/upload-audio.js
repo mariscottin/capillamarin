13a59cb729bfde956c83f5daf56a2d4ce054118e
mariscottin <nicolas.mariscotti93@gmail.com>
2019-09-17 16:23:08 -0300

    adding axios post request

--- assets/js/upload-audio.js
+++ assets/js/upload-audio.js
@@ -11,5 +11,27 @@ let newAudioForm = document.getElementById('new-audio-form');
 submitAudio.addEventListener('click', (e)=> {
     e.preventDefault();
     console.log('clicked');
+    if(audioTitle.value === "" || audioFile.value === "" || audioDate.value === ""){
+        alert('Por favor, completar todos los campos');
+      }else{
+        submitAudio.style.display = 'none';
+        loadingAudioBtn.style.display = 'block';
     
+        let formData = new FormData(newAudioForm);
+    
+        axios({
+              method: 'post',
+              url: '/audios',
+              data: formData,
+          })
+           .then(response => {
+              console.log(response);
+              if(response){
+                window.location = '/admin/homilias/1?alert=Novedad%20creada%20con%20exito';
+              }
+      
+            }).catch(error => {
+              console.log(error);
+            });
+      }
   });
