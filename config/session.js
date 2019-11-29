829a018214ded0447cd1683fb521091fa11c1186
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2019-11-29 09:55:40 -0300

    horarios table 1

--- config/session.js
+++ config/session.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     });
 
     app.use(session({
-        secret: process.env.SECRET_SESSION, //'secretcat', 
+        secret: process.env.SECRET_SESSION,  
         cookie: {
             maxAge: 86400000 //24 hours
         },
